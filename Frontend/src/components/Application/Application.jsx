ede48f99428846ba0e0817fc25f8bda3115f5cfe
Aryaman3007 <Aryaman3007>
2024-07-23 23:07:02 +0530

    Updating the render url in frontend

--- Frontend/src/components/Application/Application.jsx
+++ Frontend/src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/postapplication",
+        "https://jobquest-d6nr.onrender.com/api/v1/application/postapplication",
         formData,
         {
           withCredentials: true,
