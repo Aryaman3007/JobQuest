083e5e11609efc43764c1225e3f2eb750bac2864
Aryaman3007 <Aryaman3007>
2024-07-25 12:07:12 +0530

    backend url

--- Frontend/src/components/Application/Application.jsx
+++ Frontend/src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/postapplication",
+        "https://jobquest-vrg9.onrender.com/api/v1/application/postapplication",
         formData,
         {
           withCredentials: true,
